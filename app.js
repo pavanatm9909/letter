6180b136ed27af4e1c5094f096e2d01b7e2262b4
shashidhar269 <shashidharreddy269@gmail.com>
2024-06-27 22:45:55 -0400

    added files

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 /* Created by Tivotal */
 
 const wrapper = document.querySelector(".wrapper");
-
+const isPlay = false;
+var audio = document.getElementById('song');
 wrapper.addEventListener("click", () => {
   wrapper.classList.toggle("open");
+  if (audio.paused) {
+    audio.play();
+    isPlay = true;
+  } else {
+    audio.pause();
+    isPlay = false;
+  }
 });
